6abf49c742f0e1cf66ec83241eb6cafe48249d60
Eduardo Gutierrez <eduardo.gutierrez643@gmail.com>
2021-03-01 14:35:24 -0600

    add create/update/destroy actions

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -4,4 +4,44 @@ class Api::ProductsController < ApplicationController
     @products = Product.all
     render "index.json.jb"
   end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+    render "show.json.jb"
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.name = params[:name] || @product.name
+    @product.materials = params[:materials] || @product.materials
+    @product.price = params[:price] || @product.price
+    @product.sale_status = params[:sale_status] || @product.sale_status
+
+    if @product.save
+      render 'show.json.jb'
+    else
+      render json: {errors: @product.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def create
+    @product = Product.new(
+      name: params[:name],
+      materials: params[:materials],
+      price: params[:price],
+      sale_status: params[:sale_status]
+    )
+
+    if @product.save
+      render 'show.json.jb'
+    else
+      render json: {errors: @product.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+    render json: {message: "Item Successfully Destroyed!"}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,11 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/products" => "products#index"
+    get "/products/:id" => "products#show"
+    patch "/products/:id" => "products#update"
+    post "/products" => "products#create"
+    delete "/products/:id" => "products#destroy"
+
+
   end
 end
